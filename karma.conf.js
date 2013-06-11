7931eef65fa1549b5a6b6838f81a7550da913578
sujay <sujaybhowmick@gmail.com>
2013-06-11 15:12:51 +0530

    added karma unit test

--- karma.conf.js
+++ karma.conf.js
@@ -1,42 +1,60 @@
 // Karma configuration
+// Generated on Sat Jun 08 2013 21:59:35 GMT+0530 (IST)
+
 
 // base path, that will be used to resolve files and exclude
 basePath = '';
 
+
 // list of files / patterns to load in the browser
 files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'app/scripts/components/angular/angular.js',
-  'app/scripts/components/angular-mocks/angular-mocks.js',
-  'app/scripts/*.js',
-  'app/scripts/**/*.js',
-  'test/mock/**/*.js',
-  'test/spec/**/*.js'
+    JASMINE,
+    JASMINE_ADAPTER,
+    REQUIRE,
+    REQUIRE_ADAPTER,
+    {pattern: 'app/scripts/controllers/*.js', included: false},
+    {pattern: 'app/scripts/services/*.js', included: false},
+    {pattern: 'app/scripts/conf/*.js', included: false},
+    {pattern: 'test/spec/controllers/*.js', included: false},
+    {pattern: 'app/scripts/components/angular-unstable/angular.js', included: false},
+    {pattern: 'app/scripts/components/angular-resource/angular-resource.js', included: false},
+    {pattern: 'app/scripts/components/angular-mocks/angular-mocks.js', included: false},
+
+    'test/spec/main-test.js'
 ];
 
+
 // list of files to exclude
-exclude = [];
+exclude = [
+  
+];
+
 
 // test results reporter to use
-// possible values: dots || progress || growl
+// possible values: 'dots', 'progress', 'junit'
 reporters = ['progress'];
 
+
 // web server port
-port = 8080;
+port = 9876;
+
 
 // cli runner port
 runnerPort = 9100;
 
+
 // enable / disable colors in the output (reporters and logs)
 colors = true;
+
 
 // level of logging
 // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
 logLevel = LOG_INFO;
 
+
 // enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+autoWatch = true;
+
 
 // Start these browsers, currently available:
 // - Chrome
@@ -48,8 +66,10 @@ autoWatch = false;
 // - IE (only Windows)
 browsers = ['Chrome'];
 
+
 // If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+captureTimeout = 60000;
+
 
 // Continuous Integration mode
 // if true, it capture browsers, run tests and exit
