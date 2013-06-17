f445d153d62e36926d0851f3193d1fd7c450b1dc
sujay <sujaybhowmick@gmail.com>
2013-06-17 23:15:39 +0530

    added phone detail e2e test

---
+++ test/e2e/phoneDetailSpec.js
@@ -0,0 +1,16 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: sujay
+ * Date: 6/17/13
+ * Time: 10:12 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+'use strict';
+
+describe('phone detail e2e Test', function() {
+    beforeEach(function(){
+        browser().navigateTo('/');
+        sleep(1);
+    });
+});
